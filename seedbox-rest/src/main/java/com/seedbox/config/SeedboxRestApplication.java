d5d8945ce7d82876651acf39b0955184fc49ba67
Charles Koh <czwkoh@gmail.com>
2015-10-03 13:47:29 +0100

    changed pom

--- seedbox-rest/src/main/java/com/seedbox/config/SeedboxRestApplication.java
+++ seedbox-rest/src/main/java/com/seedbox/config/SeedboxRestApplication.java
@@ -1,11 +1,16 @@
 package com.seedbox.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SeedboxRestApplication extends SpringBootServletInitializer {
 
     @Override
